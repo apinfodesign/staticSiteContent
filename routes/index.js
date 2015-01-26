02f5f78ee8e4f6f1176dd89ffe1499997bc15a3b
My Name <me@example.com>
2015-01-26 11:36:33 -0800

    import paramaterized routing from previous project

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+////added from 11a ////////////
+var posts = require('./posts');
+
+router.param('post_name', function(request, response, next, post_name){
+    //note: we'll define posts.find in a minute
+    posts.find(post_name, function(post) {
+        if(post) {
+            request.post = post;
+            next();
+        } else {
+            return next(new Error('no such post as ' + post_name + '!'))
+        }
+    });
+});
+
+router.get('/posts/:post_name', function(request, response) {
+    response.render('post', {post: request.post});
+});
+////////////// added from 11subla
+
 module.exports = router;
