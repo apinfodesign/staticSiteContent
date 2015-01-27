921a8c68d5267a61cb04df4ac17446b935c1c772
My Name <me@example.com>
2015-01-26 17:10:57 -0800

    first day of work, read files into array.  Does not yet render via Jade

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
+//var posts = require('..posts');   //imported from my first blog
+var fs = require('fs');				 //imported from my first blog
+var jade = require('jade');				 //imported from my first blog
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Full Screen', collectorArray: fileCollector });
+
 });
 
-////added from 11a ////////////
-var posts = require('./posts');
+router.get('/pagecontent', function(req, res, next){
+   res.render('ourprojects', { title: 'Full Screen' }); 
+});
 
-router.param('post_name', function(request, response, next, post_name){
-    //note: we'll define posts.find in a minute
-    posts.find(post_name, function(post) {
-        if(post) {
-            request.post = post;
-            next();
+
+//////added from 11a ////////////
+
+var find = function(post_name, cb) {
+    fs.readdir('./posts/', function(err, files) {
+
+    	console.log(files);
+
+        if (files.indexOf(post_name + '.jade') !== -1) {
+            fs.readFile(__dirname + '/../posts/' + post_name + '.jade', function(err, data) {
+                cb(jade.compile(data)() );
+            })
         } else {
-            return next(new Error('no such post as ' + post_name + '!'))
+            cb(null);
         }
-    });
+    })
+}
+
+var blogPostFiles = fs.readdirSync(__dirname + '/../posts' ); 
+var collector=[];
+var fileCollector=[];
+//console.log("blogpostfiles " + blogPostFiles);
+
+////////////////////////////////
+blogPostFiles.forEach(function cleanFiles (value, index, array){
+	var infile;
+	var pattern = new RegExp(".jade");
+
+	if ( pattern.test(value)  )  // file IS dot jade
+		{
+		
+		inFile= fs.readFileSync( __dirname + '/../posts/'+ blogPostFiles[index]  );
+		
+		inFile = jade.render(inFile);
+		
+		fileCollector.push(inFile);
+		
+		collector.push(value);
+		console.log("Is a dot jade file.");}
+
+	else
+		{ 
+		  console.log("DID NOT FIND dot jade FILE."); 
+		}
+//console.log(collector);
+
+	
+return fileCollector;
 });
 
-router.get('/posts/:post_name', function(request, response) {
-    response.render('post', {post: request.post});
-});
-////////////// added from 11subla
+//console.log(" collector is " + collector);
+//console.log(collector.length + " is length");
+console.log("fileCollector is " + fileCollector);
+console.log(fileCollector.length + " is fileCollector length");
+
+
 
 module.exports = router;
