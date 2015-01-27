6a1a591425f2cd15819e5a546518b130773d6014
Miles Hochstein <miles.hochstein@gmail.com>
2015-01-27 10:51:59 -0800

    put forEach inside new function gatherPostFiles and use gatherPostFiles in router.get call, instead of array name.  :-)

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var jade = require('jade');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Full Screen', collectorArray: fileCollector });
+  res.render('index', { title: 'Full Screen', collectorArray: gatherPostFiles() });
 });
 
 /* GET contact page */
@@ -21,48 +21,37 @@ router.get('/bootstraptest', function(req, res, next){
    res.render('../public/indexbootstrap', { title: 'Full Screen' }); 
 }); 
 
-// regex find all .jade content files in posts directory 
-// var find = function(post_name, cb) {
-//     fs.readdir('./posts/', function(err, files) {
-//     	//console.log(files);
-//         if (files.indexOf(post_name + '.jade') !== -1) {
-//             fs.readFile(__dirname + contentFilepath + post_name + '.jade', function(err, data) {
-//                 cb(jade.compile(data)() );
-//             })
-//         } 
-//         else
-//         {
-//         cb(null);
-//         }
-//     })
-// };
+var gatherPostFiles = function(){   
+	// intentionally outside of forEach scope
+	var blogPostFiles = fs.readdirSync(__dirname + '/../posts' ); 
+	var collector=[];      //collects file names
+	var fileCollector=[];  //collects file contents
+	////////////////////////////////
+	// regex find all .jade content files in posts directory 
+	blogPostFiles.forEach(function cleanFiles (value, index, array){
+		var infile;  //takes fs incoming file
+		var pattern = new RegExp(".jade");   //match file .jade
+		if ( pattern.test(value)  )  // file IS dot jade
+			{
+			inFile= fs.readFileSync( __dirname + contentFilepath + blogPostFiles[index]  );
+			inFile = jade.render(inFile);   //make jade into html
+			fileCollector.push(inFile);    //keep file content		
+			collector.push(value);         //keep file name, not really needed?
+			//console.log("Is a dot jade file.");
+			}
+		else
+			{ //console.log("DID NOT FIND dot jade FILE."); 
+			}
+	return fileCollector;
+	});
+	//console.log(" collector is " + collector);
+	//console.log(collector.length + " is length");
+	//console.log("fileCollector is " + fileCollector);
+	//console.log(fileCollector.length + " is fileCollector length");
 
-// intentionally outside of forEach scope
-var blogPostFiles = fs.readdirSync(__dirname + '/../posts' ); 
-var collector=[];      //collects file names
-var fileCollector=[];  //collects file contents
-
-////////////////////////////////
-blogPostFiles.forEach(function cleanFiles (value, index, array){
-	var infile;  //takes fs incoming file
-	var pattern = new RegExp(".jade");   //match file .jade
-	if ( pattern.test(value)  )  // file IS dot jade
-		{
-		inFile= fs.readFileSync( __dirname + contentFilepath + blogPostFiles[index]  );
-		inFile = jade.render(inFile);   //make jade into html
-		fileCollector.push(inFile);    //keep file content		
-		collector.push(value);         //keep file name, not really needed?
-		//console.log("Is a dot jade file.");
-		}
-	else
-		{ //console.log("DID NOT FIND dot jade FILE."); 
-		}
 return fileCollector;
-});
-//console.log(" collector is " + collector);
-//console.log(collector.length + " is length");
-//console.log("fileCollector is " + fileCollector);
-//console.log(fileCollector.length + " is fileCollector length");
+};  //close gatherPostFiles
 
+//gatherPostFiles();
 
 module.exports = router;
